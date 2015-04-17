999d0e53d939609e60a82f891ffb46c7f42c34bd
Chris Dutton <chris@rosepointnav.com>
2015-04-16 22:42:08 -0700

    Updated to VERSION file

--- mix.exs
+++ mix.exs
@@ -4,13 +4,22 @@ defmodule Hub.Mixfile do
 
   def project, do: [
     app: :hub,
-    version: "0.8.0",
+    version: version,
     elixir: "~> 1.0",
     source_url: "https://github.com/cellulose/hub",
     homepage_url: "http://cellulose.io",
     deps: deps
   ]
 
-  defp deps, do: []
+  defp deps, do: [
+    {:earmark, "~> 0.1", only: :dev},
+    {:ex_doc, "~> 0.7", only: :dev}
+  ]
 
+  defp version do
+    case File.read("VERSION") do
+      {:ok, ver} -> String.strip ver
+      _ -> "0.0.0-dev"
+    end
+  end
  end
